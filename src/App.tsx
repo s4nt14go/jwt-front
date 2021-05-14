674940a14718d7d85b8108b170736771badb8e2d
s4nt14go <santiagopergamo@gmail.com>
2021-05-13 22:11:19 -0300

    feat: disable create account button when no account is entered

--- src/App.tsx
+++ src/App.tsx
@@ -147,7 +147,7 @@ function App() {
                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <button onClick={create}
-              className="w-full sm:w-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Create
+              className={`${!fields.account? 'opacity-50 cursor-not-allowed' : ''} w-full sm:w-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg`}>Create
             </button>
           </div>
 
